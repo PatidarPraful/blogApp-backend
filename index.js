ee5da8b64e0f66d0e5f164c85e2a329b6dcaf2dd
prafull_30 <prafullpop30082003@gmail.com>
2025-04-02 14:25:16 +0530

    correcting url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors({ origin: [
     "http://localhost:5173", // Local frontend (for development)
-    "https://vercel.com/prafull-patidars-projects/blog-app-frontend-s54f" // Vercel frontend
+    "https://blog-app-frontend-s54f.vercel.app"// Vercel frontend
 ],credentials:true}))
 
 
